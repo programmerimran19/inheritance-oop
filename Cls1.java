128cce98540cdc3d84fedbc7b49ad3bb98bc4b71
Programmer Imran <38666739+programmerimran19@users.noreply.github.com>
2021-10-10 12:52:13 +0600

    Add files via upload

---
+++ Cls1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cls1;
+
+/**
+ *
+ * @author Programmer Imran
+ */
+public class Cls1 
+{
+
+    void add(int p,int q)
+    {
+        System.out.println(p+q);
+    }
+    public static void main(String[] args) 
+    {
+        Cls2 C = new Cls2();
+        C.add(4, 4);
+        C.mul(4, 4);
+        C.task(4, 4);
+    }
+    
+}
---
+++ Cls2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cls1;
+
+/**
+ *
+ * @author Programmer Imran
+ */
+public class Cls2 extends Cls1{
+    
+    void mul(int p,int q)
+    {
+        System.out.println(p*q);
+    }
+    
+    void task(int a,int b)
+    {
+        System.out.println((a*a)+(b*b));
+    }
+    
+}
